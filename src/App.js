18a238dd3c44515d2eb3ae5e71c47c4b982e68d3
Van <foxrainsad@gmail.com>
2025-01-02 11:23:35 +0700

    #1: Design Header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import './App.scss';
+import Header from './components/Header/Header';
 
 const App = () => {
   return (
     <div className="app-contrainer">
-      Hello world
+      <Header />
     </div>
   );
 }
