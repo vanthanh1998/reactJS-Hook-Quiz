966e3df328d414b61e05806b68aeed6cf19d2e12
Van <foxrainsad@gmail.com>
2025-01-03 16:21:28 +0700

    #5: customize axios => call api

--- src/components/Content/ModalCreateUser.js
+++ src/components/Content/ModalCreateUser.js
@@ -2,9 +2,9 @@ import { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { FcPlus } from "react-icons/fc";
-import axios from "axios";
 import React from "react";
 import { toast } from "react-toastify";
+import { postCreateNewUser } from "../../sevices/apiServies";
 
 const ModalCreateUser = (props) => {
   const { show, setShow } = props;
@@ -56,24 +56,13 @@ const ModalCreateUser = (props) => {
     }
 
     // call api submit data
-    const data = new FormData();
-    data.append("email", email);
-    data.append("password", password);
-    data.append("username", username);
-    data.append("role", role);
-    data.append("userImage", image);
-
-    let res = await axios.post(
-      "http://localhost:8081/api/v1/participant",
-      data
-    );
-
-    if (res.data && res.data.EC === 0) {
-      toast.success(res.data.EM);
+    let data = await postCreateNewUser(email, password, username, role, image);
+    if (data && data.EC === 0) {
+      toast.success(data.EM);
       handleClose();
     }
-    if (res.data && res.data.EC !== 0) {
-      toast.error(res.data.EM);
+    if (data && data.EC !== 0) {
+      toast.error(data.EM);
     }
   };
 
