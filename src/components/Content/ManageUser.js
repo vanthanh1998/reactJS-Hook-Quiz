b864947bc9cbc74b9369d44af59feb2117b6e8d0
Van <foxrainsad@gmail.com>
2025-01-04 19:39:28 +0700

    #7: update users when create new user success => not reload page

--- src/components/Content/ManageUser.js
+++ src/components/Content/ManageUser.js
@@ -1,11 +1,24 @@
 import ModalCreateUser from "./ModalCreateUser";
 import "./ManageUser.scss";
 import { FcPlus } from "react-icons/fc";
-import { useState } from "react";
 import TableUser from "./TableUser";
+import { useEffect, useState } from "react";
+import { getAllUsers } from "../../sevices/apiServies";
 
 const ManageUser = (props) => {
   const [showModalCreateUser, setShowModalCreateUser] = useState(false);
+
+  const [listUsers, setListUsers] = useState([]);
+  useEffect(() => {
+    fetchListUsers();
+  }, []);
+
+  const fetchListUsers = async () => {
+    let res = await getAllUsers();
+    if (res.EC === 0) {
+      setListUsers(res.DT);
+    }
+  };
   return (
     <div className="manage-user-container">
       <div className="title">Manage User</div>
@@ -19,11 +32,12 @@ const ManageUser = (props) => {
           </button>
         </div>
         <div className="table-users-container">
-          <TableUser />
+          <TableUser listUsers={listUsers} />
         </div>
         <ModalCreateUser
           show={showModalCreateUser}
           setShow={setShowModalCreateUser}
+          fetchListUsers={fetchListUsers}
         />
       </div>
     </div>
