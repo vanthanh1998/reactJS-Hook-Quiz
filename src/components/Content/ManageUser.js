c8c1cfc068b1c2b024c469f3d2b705bdbf0e86e9
Van <foxrainsad@gmail.com>
2025-01-04 19:28:12 +0700

    #6: Show list all users

--- src/components/Content/ManageUser.js
+++ src/components/Content/ManageUser.js
@@ -2,6 +2,7 @@ import ModalCreateUser from "./ModalCreateUser";
 import "./ManageUser.scss";
 import { FcPlus } from "react-icons/fc";
 import { useState } from "react";
+import TableUser from "./TableUser";
 
 const ManageUser = (props) => {
   const [showModalCreateUser, setShowModalCreateUser] = useState(false);
@@ -17,7 +18,9 @@ const ManageUser = (props) => {
             <FcPlus /> Add new users
           </button>
         </div>
-        <div className="table-users-container">Table users</div>
+        <div className="table-users-container">
+          <TableUser />
+        </div>
         <ModalCreateUser
           show={showModalCreateUser}
           setShow={setShowModalCreateUser}
--- src/sevices/apiServies.js
+++ src/sevices/apiServies.js
@@ -10,4 +10,8 @@ const postCreateNewUser = (email, password, username, role, image) => {
   return axios.post("api/v1/participant", data);
 };
 
-export { postCreateNewUser };
+const getAllUsers = () => {
+  return axios.get("api/v1/participant/all");
+};
+
+export { postCreateNewUser, getAllUsers };
